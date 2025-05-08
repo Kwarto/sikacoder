1b36beda8975c4ea25b8d30530a61a82c7cc294f
bluspacedev <bluspacedev@gmail.com>
2025-05-08 14:05:13 +0000

    Added task and setting component > task

--- src/components/Overview/messages/Messages.jsx
+++ src/components/Overview/messages/Messages.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import msgAvaImg from '../../../assets/images/avatar.png'
 import { FaDroplet, FaEye } from 'react-icons/fa6'
+// import { motion } from 'framer-motion'
+
 const Messages = () => {
   return (
     <MessageContainerWrapper>
