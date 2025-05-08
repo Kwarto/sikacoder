0aba4455231256a58889b7921e77553d6c8ae0ad
bluspacedev <bluspacedev@gmail.com>
2025-05-08 00:56:35 +0000

    Added messages component

--- src/components/Overview/messages/Messages.jsx
+++ src/components/Overview/messages/Messages.jsx
@@ -4,7 +4,11 @@ import styled from 'styled-components'
 const Messages = () => {
   return (
     <MessageContainerWrapper>
+     <MessagesCardWrapper>
+      <MessagesCard className='card'>
 
+      </MessagesCard>
+     </MessagesCardWrapper>
     </MessageContainerWrapper>
   )
 }
@@ -12,7 +16,22 @@ const Messages = () => {
 const MessageContainerWrapper = styled.article`
  width: 100%;
  height: 100%;
- background: cyan;
+ background: #f1f1f1;
+`
+const MessagesCardWrapper = styled.div`
+ width: 100%;
+ height: 100%;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ flex-direction: column;
+`
+const MessagesCard = styled.div`
+ width: 80%;
+ height: 120px;
+ background: rgba(255,255,255.06);
+ backdrop-filter: blur(10px);
+ border-radius: 10px;
 `
 
 export default Messages
