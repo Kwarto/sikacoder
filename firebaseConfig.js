85054a618523383969fc96e019bf8a23ea396871
Kwarteng Joseph <108136477+Kwarto@users.noreply.github.com>
2025-07-03 12:48:47 +0000

    Resolving errs after adding .env v1.0.0

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -11,8 +11,7 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_APP_PROJECT_ID,
   storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_appId
-
+  appId: import.meta.env.VITE_APP_APP_ID
 }
 
 // Initialize Firebase
